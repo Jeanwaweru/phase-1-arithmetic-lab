7368160efd4fcf60b4ee4421254ef670a40ac293
Jean <jean.wachuka@student.moringaschool.com>
2024-03-31 23:24:22 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//Multiply num1 and num2 
+const num1= 62;
+const num2= 1;
+const multiply= num1* num2;
 
-// Write your code here
+// Generating a random integer greater than 0
+const random = Math.floor(Math.random ()* 100) +1;
+
+//Calculate the remainder of num3 divided by num4
+const num3= 22;
+const num4= 6;
+const mod= num3 % num4;
+
+//Find the highest number in a set
+const set =[8,16,20,10,7,];
+const max =Math.max(...set);
